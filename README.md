c424970c25626ee2d7ad045f8a57e33aba3b2c14
er-knight <menobleknight@gmail.com>
2021-09-03 11:05:01 +0530

    add guess the number game in lua

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ## `🔎 guess the number`
 "guess the number" game in various programming languages
+
+## 📚 references
+- [Learn Lua by writing a "guess the number" game](https://opensource.com/article/20/12/lua-guess-number-game) by [Seth Kenlon](https://opensource.com/users/seth) on opensource.com
+
---
+++ guess.lua
@@ -0,0 +1,25 @@
+math.randomseed(os.time())
+number = math.random(1, 100)
+	
+player = {}
+player.guess = 0
+
+-- print(number)
+
+print("Guess a number between 1 and 100")
+
+while ( player.guess ~= number ) do
+	io.write("> ")
+  	player.answer = io.read()
+  	player.guess = tonumber(player.answer)
+	
+	if ( player.guess > number ) then
+    	print("Too high")
+  	elseif ( player.guess < number) then
+    	print("Too low")
+  	else
+    	print("That's right!")
+    	break
+  	end
+	
+end
