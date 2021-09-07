b4cd142965e296f886ed1a7bbdd33c1567ad17bf
er-knight <menobleknight@gmail.com>
2021-09-07 22:24:00 +0530

    add guess the number in go

---
+++ guess.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	number := rand.Intn(100 - 1) + 1
+
+	guess := 0
+
+	fmt.Println("guess the number between 1 and 100")
+
+	for guess != number {
+		fmt.Print("> ")
+		fmt.Scanln(&guess)
+
+		if guess < number {
+			fmt.Println("too low!")
+		} else if guess > number {
+			fmt.Println("too high!")
+		}
+	}
+	fmt.Println("that's right!")
+}
