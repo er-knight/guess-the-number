124a246ce27d80f12af51262aef18d77eee083c9
er-knight <menobleknight@gmail.com>
2021-09-07 15:22:33 +0530

    add guess the number game in kotlin

---
+++ guess.kt
@@ -0,0 +1,23 @@
+fun main() {
+	val number = (1..100).random()
+
+	println("Guess a number between 1 and 100")
+
+	var guess = 0
+	
+    while (guess != number) {
+		print("> ")
+		guess = readLine()!!.toInt()
+
+		if (guess > number) {
+			println("Too high")
+		}
+		else if (guess < number) {
+			println("Too low")
+		}
+		else {
+			println("That's right!")
+			break 
+		}
+	}
+}
